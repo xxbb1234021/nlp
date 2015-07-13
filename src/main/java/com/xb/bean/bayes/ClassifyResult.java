18a5d2a3d6c2977149ecdca5e57396a1f776f4bd
kevin.xb <xxbb1234021@qq.com>
2015-07-13 17:48:08 +0800

    add bayes

---
+++ src/main/java/com/xb/bean/bayes/ClassifyResult.java
@@ -0,0 +1,14 @@
+package com.xb.bean.bayes;
+/**
+* 分类结果
+*/
+public class ClassifyResult 
+{
+    public double probility;//分类的概率
+    public String classification;//分类
+    public ClassifyResult()
+    {
+        this.probility = 0;
+        this.classification = null;
+    }
+}
